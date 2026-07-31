9f32fcfb72a60bad71d31bd1041b4ab9d5a35805
paolashultz <paolashultz@gmail.com>
2026-07-31 18:41:37 +0200

    feat: add prepared six-operator primitives

--- src/dsp/six_op_pm/mod.rs
+++ src/dsp/six_op_pm/mod.rs
@@ -1,1 +1,8 @@
 pub mod algorithm;
+pub mod envelope;
+pub mod operator;
+
+pub use envelope::{Envelope, EnvelopeError, EnvelopeSpec};
+pub use operator::{
+    FrequencyMode, KeyboardScaling, OperatorError, OperatorSpec, PreparedOperator, SineTable,
+};
